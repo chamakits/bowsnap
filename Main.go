b1d3140619e8a1496709809d8360cbd36425e31b
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2013-09-21 18:52:39 -0400

    Added some changes for gitignore.
    Added flag init logic and flag validation.
    
    Added logic to save a json snapshot, based on snapshot.
    Creating symlink to the newly created json file.

---
+++ Main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/chamakits/bowsnap/snap"
+	"os"
+	"time"
+)
+
+func main() {
+	var (
+		serverFlag          bool
+		snapshotVersionFlag string
+		newSnapshotFlag     bool
+		newSnapshotNameFlag string
+		repoUrlFlag         string
+	)
+
+	initFlags(&serverFlag, &snapshotVersionFlag, &newSnapshotFlag, &newSnapshotNameFlag, &repoUrlFlag)
+
+	if serverFlag {
+		//startServer(snapshotVersionFlag)
+	} else if newSnapshotFlag {
+		snap.TakeNewSnapshot(repoUrlFlag, newSnapshotNameFlag)
+	}
+
+	fmt.Println(serverFlag)
+	fmt.Println(snapshotVersionFlag)
+	fmt.Println(newSnapshotFlag)
+	fmt.Println(newSnapshotNameFlag)
+}
+
+const bowerDefaultRepo = "http://bower.herokuapp.com/packages"
+
+func initFlags(serverFlag *bool, snapshotVersionFlag *string, newSnapshotFlag *bool, newSnapshotNameFlag *string, repoUrlFlag *string) {
+	//Init flag setup
+	flag.BoolVar(serverFlag, "s", false, "Run the mirrored bower server.")
+	flag.StringVar(snapshotVersionFlag, "sname", "LATEST", "OPTIONAL:Choose version of snapshot to serve.  When used, must be with the \"s\" flag as well.")
+
+	flag.BoolVar(newSnapshotFlag, "n", false, "Create a new snapshot of bower.")
+	flag.StringVar(newSnapshotNameFlag, "nname", "", "OPTIONAL: Name for new snapshot to copy. When used, must be with the \"n\" flag. ")
+	flag.StringVar(repoUrlFlag, "repo", "", "OPTIONAL: Url of the bower repo to take snapshot from. When used, must be with the \"n\" flag. ")
+
+	flag.Parse()
+
+	//Do flag validation
+	if len(os.Args) <= 1 {
+		*serverFlag = true
+	} else if *serverFlag && *newSnapshotFlag {
+
+	} else if *serverFlag && len(*snapshotVersionFlag) < 1 {
+		//*snapshotVersionFlag="LATEST"
+		fmt.Fprintf(os.Stderr, "ERROR:  Tried to run program in \"server\" mode while providing a blank \"version\".\n")
+		flag.Usage()
+		fmt.Fprintf(os.Stderr, "ERROR:  Exiting because of incorrect flag.\n")
+		os.Exit(1)
+	} else if *newSnapshotFlag && len(*newSnapshotNameFlag) < 1 {
+		*newSnapshotNameFlag = getDefSnapshotName()
+	}
+	if *newSnapshotFlag && len(*repoUrlFlag) < 1 {
+		*repoUrlFlag = bowerDefaultRepo
+	}
+
+}
+
+const timeLayout = "Jan-02-2006_15-04-05-MST"
+
+func getDefSnapshotName() string {
+	now := time.Now()
+	return "BowerSnapshot_" + now.Format(timeLayout) + ".json"
+}
---
+++ snap/Snapshot.go
@@ -0,0 +1,73 @@
+package snap
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+)
+
+func TakeNewSnapshot(repoUrl string, newSnapshotNameFlag string) {
+	resp, err := http.Get(repoUrl)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR:  Could not reach bower repository URL at \"%s\".\n", repoUrl)
+		fmt.Fprintf(os.Stderr, "ERROR-MESSAGE:%v\n", err)
+		os.Exit(2)
+	}
+	readBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR:  Could not read response from repository URL at \"%s\".\n", repoUrl)
+		fmt.Fprintf(os.Stderr, "ERROR-MESSAGE:%v\n", err)
+		os.Exit(3)
+	}
+	//fmt.Println(string(readBytes))
+	currentWorkingDirectory := getCWD()
+	writeFile(&readBytes, currentWorkingDirectory, newSnapshotNameFlag)
+	createLatestSymlink(currentWorkingDirectory, newSnapshotNameFlag)
+
+}
+
+func getCWD() string {
+	currentWorkingDirectory, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR:  Could not get working directory.\n")
+		fmt.Fprintf(os.Stderr, "ERROR-MESSAGE:%v\n", err)
+		os.Exit(4)
+	}
+	return currentWorkingDirectory
+}
+
+func writeFile(readBytes *[]byte, currentWorkingDirectory string, newFileName string) {
+	snapshotFilePath := path.Join(currentWorkingDirectory, newFileName)
+	ioutil.WriteFile(snapshotFilePath, *readBytes, 0644)
+}
+
+func createLatestSymlink(currentWorkingDirectory string, newFileName string) {
+	symlinkPath := path.Join(currentWorkingDirectory, "LATEST.json")
+	snapshotFilePath := path.Join(currentWorkingDirectory, newFileName)
+	_, err := os.Lstat(symlinkPath)
+	var linkPathExists, isLink bool
+	//Checking that the link's path exists.  Doesn't guarantee its a symlink though, just that the file exists.
+	if err == nil {
+		linkPathExists = true
+	}
+
+	//If the file exists, now check if it is actually a symlink.
+	if linkPathExists {
+		_, err = os.Readlink(symlinkPath)
+		if err == nil {
+			isLink = true
+		} else {
+			fmt.Fprintf(os.Stderr, "ERROR:  Found existing file which is not a symlink at path \"%s\".\n", symlinkPath)
+			fmt.Fprintf(os.Stderr, "ERROR-MESSAGE:%v\n", err)
+			os.Exit(5)
+		}
+	}
+
+	if linkPathExists && isLink {
+		os.Remove(symlinkPath)
+	}
+
+	os.Symlink(snapshotFilePath, symlinkPath)
+}
